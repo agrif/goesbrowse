a6553fe5b9207a2a1a048b7b164cbba7774bde21
Aaron Griffith <aargri@gmail.com>
2019-07-04 20:28:55 -0400

    fixed timelapse command (lol)

--- goesbrowse/__main__.py
+++ goesbrowse/__main__.py
@@ -2,6 +2,7 @@ import click
 import flask
 
 import goesbrowse.application
+import goesbrowse.database
 
 @click.group(cls=flask.cli.FlaskGroup, create_app=lambda scriptinfo: goesbrowse.application.app)
 @click.option('--config')
@@ -28,17 +29,16 @@ def timelapse():
     postroll = 5
     rate = 1 / (8 * 60 * 60)
 
-    files = appdb.get_files(filters=dict(region='FD', source='GOES16', channel='CH13_enhanced'))
-    files.reverse()
+    files = goesbrowse.database.File.query.filter_by(region='FD', source='GOES16', channel='FC').order_by(goesbrowse.database.File.date)
     lastdate = None
     print('ffconcat version 1.0')
     for f in files:
         if lastdate is not None:
-            duration = f['date'] - lastdate
+            duration = f.localdate - lastdate
             print('duration {}'.format(duration.total_seconds() * rate))
-        print('file {}'.format(f['datapath']))
-        print('file_packet_metadata imagedate=\'{:%a %b %d %Y, %H:%M:%S %Z}\''.format(f['date']))
-        lastdate = f['date']
+        print('file {}'.format(f.datapath))
+        print('file_packet_metadata imagedate=\'{:%a %b %d %Y, %H:%M:%S %Z}\''.format(f.localdate))
+        lastdate = f.localdate
     print('duration {}'.format(postroll))
 
 if __name__ == '__main__':
